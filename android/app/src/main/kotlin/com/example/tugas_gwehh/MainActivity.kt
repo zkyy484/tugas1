14ac952310d1167344b7790ad32af7e8a9d335bb
zkyy484 <dzakymusyaddad@gmail.com>
2024-09-30 12:27:48 +0700

    tampilan logo

---
+++ android/app/src/main/kotlin/com/example/tugas_gwehh/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tugas_gwehh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/basic_widget/loading_cupertino.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class LoadingCupertino extends StatelessWidget {
+  @override 
+    Widget build(BuildContext context) {
+      return MaterialApp(
+      home: Container(
+        margin: const EdgeInsets.only(top: 30),
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            CupertinoButton(
+              child: const Text("Contoh button"),
+              onPressed: () {},
+            ),
+            const SizedBox(height: 15,),
+            const CupertinoActivityIndicator(),
+          ],
+        ),
+      ),
+    );
+
+    }
+
+}
